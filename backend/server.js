e2847b946fcc256b4d9a0d2ccc2fdd52b847f1f0
dci-student <yasaman.nikpay@gmail.com>
2024-10-08 13:23:19 +0200

    second commit

--- backend/server.js
+++ backend/server.js
@@ -4,26 +4,26 @@ import todosRoutes from "./routes/todos.js";
 import { logger } from "./middleware/loggerMiddleware.js";
 
 const app = express();
-const port = 3000;
+const port = 3005;
 
 // Middleware
 app.use(express.json());
 app.use(cors());
-app.use(logger); 
+app.use(logger);
 app.use("/api/todos", todosRoutes);
 
-// globale Fehlerbehandlung
+// Global Error Handling
 app.use((err, req, res, next) => {
-  console.error("error")
-  res.status(500).json({message: 'Something went wrong'})
-})
+  console.error(err.message); 
+  res.status(500).json({ message: "Something went wrong" });
+});
 
-// default route
+// Default Route
 app.use((req, res) => {
   res.status(404).send("Not found");
-})
-
+});
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
+
